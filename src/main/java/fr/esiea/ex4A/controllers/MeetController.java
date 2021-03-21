204e14a4688fb79552ab53905fc72e23ef5e1d5f
Hector GODZIEN <hgodzien@gmail.com>
2021-03-21 23:25:00 +0100

    Setuped all classes for completing part2 of project (JSON API working with fake datas)

--- src/main/java/fr/esiea/ex4A/controllers/MeetController.java
+++ src/main/java/fr/esiea/ex4A/controllers/MeetController.java
@@ -1,38 +1,48 @@
+/* (Copyrights Hector GODZIEN) 2021 */
 package fr.esiea.ex4A.controllers;
 
+import fr.esiea.ex4A.models.Match;
 import fr.esiea.ex4A.models.User;
 import fr.esiea.ex4A.services.MeetService;
+import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Controller
 @RequestMapping(path = "/api")
 public class MeetController {
 
-    @Autowired
-    private final MeetService service;
+    @Autowired private final MeetService service;
 
-    public MeetController(MeetService service) {
-        this.service = service;
-    }
+    public MeetController(MeetService service) { this.service = service; }
 
     @GetMapping(path = "/users", produces = "application/json")
-    public List<User> getAllUsers() {
-        return this.service.getAllUsers();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(this.service.getAllUsers(), HttpStatus.OK);
     }
 
-    @PostMapping(path = "/inscription", produces = "application/json")
-    public ResponseEntity<?> signUp(@RequestBody User user) {
-        return null;
+    @PostMapping(
+            path = "/inscription",
+            produces = "application/json",
+            consumes = "application/json")
+    public ResponseEntity<?> signUp(@Valid @RequestBody User user) {
+        this.service.addUser(user);
+        if (this.service.getAllUsers().contains(user)) {
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        }
+        return new ResponseEntity<>(user, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @GetMapping(path = "/matches")
-    public ResponseEntity<?> getMatches(@RequestParam(value = "userName") String username, @RequestParam(value = "userCountry") String country) {
-        return null;
+    @GetMapping(path = "/matches", produces = "application/json")
+    public ResponseEntity<?> getMatches(
+            @NotNull @NotBlank @RequestParam(value = "userName") String username,
+            @NotNull @NotBlank @RequestParam(value = "userCountry") String country) {
+        return new ResponseEntity<>(new Match("jacklin", "@yourSweetHeart45"), HttpStatus.OK);
     }
-
 }
--- src/main/java/fr/esiea/ex4A/services/MeetService.java
+++ src/main/java/fr/esiea/ex4A/services/MeetService.java
@@ -1,17 +1,16 @@
+/* (Copyrights Hector GODZIEN) 2021 */
 package fr.esiea.ex4A.services;
 
 import fr.esiea.ex4A.models.User;
 import fr.esiea.ex4A.repositories.MeetRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class MeetService {
 
-    @Autowired
-    private final MeetRepository repository;
+    @Autowired private final MeetRepository repository;
 
     public MeetService(MeetRepository repository) {
         this.repository = repository;
@@ -19,5 +18,9 @@ public class MeetService {
 
     public List<User> getAllUsers() {
         return this.repository.getAllUsers();
+    }
+
+    public void addUser(User user) {
+        this.repository.addUser(user);
     }
 }
