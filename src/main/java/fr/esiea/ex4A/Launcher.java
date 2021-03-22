76f12b1b2b384ead770957028d81ca4856b17d41
Hector GODZIEN <hgodzien@gmail.com>
2021-03-22 01:13:21 +0100

    Add test-integration phase to JaCoCo plugin to add integration tests to the report

--- src/main/java/fr/esiea/ex4A/Launcher.java
+++ src/main/java/fr/esiea/ex4A/Launcher.java
@@ -2,11 +2,23 @@ package fr.esiea.ex4A;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import retrofit2.Retrofit;
 
 @SpringBootApplication
 public class Launcher {
 
     public static void main(String[] args) {
         SpringApplication.run(Launcher.class, args);
+    }
+
+    @Bean
+    AgifyClient agifyClient() {
+        Retrofit retrofit = Retrofit.Builder()
+            .baseUrl("https://api.agify.io")
+            .build();
+
+        return retrofit.create(AgifyClient.class);
     }
 }
