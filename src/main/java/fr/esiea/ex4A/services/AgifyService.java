a08c5c591885fb7995109e1aa9dd852f04edd8c6
Hector GODZIEN <hgodzien@gmail.com>
2021-03-22 01:44:33 +0100

    Added 'caching' feature (it won't make API calls if a user exist with the requested username and country)

--- src/main/java/fr/esiea/ex4A/services/AgifyService.java
+++ src/main/java/fr/esiea/ex4A/services/AgifyService.java
@@ -2,6 +2,8 @@
 package fr.esiea.ex4A.services;
 
 import fr.esiea.ex4A.clients.AgifyClient;
+import fr.esiea.ex4A.models.User;
+import fr.esiea.ex4A.repositories.MeetRepository;
 import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,13 +12,19 @@ import org.springframework.stereotype.Service;
 public class AgifyService {
 
     @Autowired private final AgifyClient agifyClient;
+    @Autowired private final MeetRepository meetRepository;
 
-    public AgifyService(AgifyClient agifyClient) {
+    public AgifyService(AgifyClient agifyClient, MeetRepository meetRepository) {
         this.agifyClient = agifyClient;
+        this.meetRepository = meetRepository;
     }
 
     public Integer getAgeByNameAndCountry(String username, String country) {
         try {
+            User u = this.meetRepository.getUserByNameAndCountry(username, country);
+            if (u != null) {
+                return u.getAge();
+            }
             return Integer.parseInt(
                     agifyClient
                             .getAgeByNameAndCountry(username, country)
